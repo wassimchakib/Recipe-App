9acd9b1af559ecf454f536e8c2b6be42f89da2e1
Ben Sales <adebsa2401@gmail.com>
2023-03-15 07:10:26 +0100

    feat: Design User views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_sign_up_params, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- db/migrate/20230314111210_devise_create_users.rb
+++ db/migrate/20230314111210_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       ## User's attributes
       t.string :name, null: false, default: ""
-      
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
