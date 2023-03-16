d727e852d536d32206b5f3fe5b2d59678397d727
Wassim <wassimchakib@hotmail.com>
2023-03-16 13:49:08 +0100

    Fix rubocop

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -8,10 +8,16 @@ RSpec.describe Recipe, type: :model do
   end
   let(:long_description) do
     'orange quote icon Wash hands with soap and water.  Lightly grease a 3-quart casserole dish and set aside.
-    In large skillet over medium-high heat, warm oil. Add shallots and sauté 1 minute. Add chicken strips and cook 5 minutes more.  orange quote icon Wash hands with soap and water after handling raw chicken.
-    Place a layer of bread cubes in the bottom of the dish. In alternating layers, add chicken, spinach, sundried tomatoes, cheese and bread, ending with a sprinkling of bread on top.
-    In a large bowl, combine eggs, milk, mustard and nutmeg with a whisk.  orange quote icon Wash hands after touching raw eggs. Pour mixture over the casserole, cover and refrigerate on lowest shelf 4 hours or overnight.  orange quote icon Wash counter after touching eggs.
-    Preheat oven to 350°F. While oven heats, remove casserole from refrigerator. Place casserole in oven and bake 35—40 minutes, until golden brown on top.  orange quote icon Casserole is done with chicken reaches 165 °F as measured with a food thermometer. Remove from oven and cool 5 minutes before serving. Serve hot.'
+    In large skillet over medium-high heat, warm oil. Add shallots and sauté 1 minute. Add chicken strips and
+    cook 5 minutes more. orange quote icon Wash hands with soap and water after handling raw chicken.
+    Place a layer of bread cubes in the bottom of the dish. In alternating layers, add chicken, spinach,
+    sundried tomatoes, cheese and bread, ending with a sprinkling of bread on top.
+    In a large bowl, combine eggs, milk, mustard and nutmeg with a whisk.  orange quote icon Wash hands
+    after touching raw eggs. Pour mixture over the casserole, cover and refrigerate on lowest shelf 4 hours
+    or overnight.  orange quote icon Wash counter after touching eggs. Preheat oven to 350°F. While oven heats,
+    remove casserole from refrigerator. Place casserole in oven and bake 35—40 minutes, until golden brown on top.
+    orange quote icon Casserole is done with chicken reaches 165 °F as measured with a food thermometer.
+    Remove from oven and cool 5 minutes before serving. Serve hot.'
   end
   it 'fails because name should be present' do
     recipe.name = nil
