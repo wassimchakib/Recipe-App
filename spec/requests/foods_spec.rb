113f7301da09d9f208cc835926cf4d5c32785e45
Wassim <wassimchakib@hotmail.com>
2023-03-17 16:14:12 +0100

    Add foods and recipes request spec

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -1,7 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe 'Foods', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'FoodsController', type: :request do
+  describe 'GET /foods/' do
+    before(:all) do
+      user = User.find_by(email: 'wassimchakib@gmail.com')
+      user.skip_confirmation!
+      login_as(user)
+      get foods_path
+    end
+
+    # Get lists of all foods
+    it 'returns a 200 status response' do
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /foods/:food_id' do
+    before(:all) do
+      user = User.find_by(email: 'wassimchakib@gmail.com')
+      user.skip_confirmation!
+      login_as(user)
+      get recipe_path(1)
+    end
+
+    # Get Specific Recipe
+    it 'returns a 200 status response' do
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+    end
   end
 end
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,7 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe 'Recipes', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'RecipesController', type: :request do
+  describe 'GET /recipes/' do
+    before(:all) do
+      user = User.find_by(email: 'wassimchakib@gmail.com')
+      user.skip_confirmation!
+      login_as(user)
+      get recipes_path
+    end
+
+    # Get lists of all recipes
+    it 'returns a 200 status response' do
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /recipes/:recipe_id' do
+    before(:all) do
+      user = User.find_by(email: 'wassimchakib@gmail.com')
+      user.skip_confirmation!
+      login_as(user)
+      get recipe_path(1)
+    end
+
+    # Get Specific Recipe
+    it 'returns a 200 status response' do
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+    end
   end
 end
