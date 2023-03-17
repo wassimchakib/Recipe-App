d47521421407331a64deffe670c3378121f1646e
Wassim <wassimchakib@hotmail.com>
2023-03-17 22:29:23 +0100

    Fix general shopping list bug

--- spec/views/general_shopping_list_index_spec.rb
+++ spec/views/general_shopping_list_index_spec.rb
@@ -10,13 +10,13 @@ RSpec.describe 'Shopping list', type: :system do
   end
 
   let(:recipe) do
-    Recipe.find_or_create_by(user:, name: 'My First Recipe', preparation_time: '15 min', cooking_time: '45 min',
-                             public: true, description: 'This is the description of recipe')
+    Recipe.create!(user:, name: 'My First Recipe', preparation_time: '15 min', cooking_time: '45 min',
+                   public: true, description: 'This is the description of recipe')
   end
 
-  let(:food) { Food.find_or_create_by(name: 'Chicken Breast', measurement_unit: 'Kg', price: 200, quantity: 2, user:) }
+  let(:food) { Food.create!(name: 'Chicken Breast', measurement_unit: 'Kg', price: 200, quantity: 2, user:) }
 
-  let(:recipe_food) { RecipeFood.find_or_create_by(food:, recipe:, quantity: 4) }
+  let(:recipe_food) { RecipeFood.create!(food:, recipe:, quantity: 4) }
 
   before(:each) do
     login_as(user)
@@ -24,33 +24,54 @@ RSpec.describe 'Shopping list', type: :system do
 
   describe 'index page' do
     it 'shows the title of the page' do
+      recipe
+      food
+      recipe_food
       visit recipe_general_shopping_list_path(recipe.id)
       expect(page).to have_content('Shopping List')
     end
 
     it 'shows the amount of food items to buy' do
+      recipe
+      food
+      recipe_food
       visit recipe_general_shopping_list_path(recipe.id)
       expect(page).to have_content('Amount of food items to buy:')
     end
 
     it 'shows the total value of food needed' do
+      recipe
+      food
+      recipe_food
       visit recipe_general_shopping_list_path(recipe.id)
       expect(page).to have_content('Total Value of food needed:')
     end
 
     it 'Shows the list of food to shop' do
+      recipe
+      food
+      recipe_food
       visit recipe_general_shopping_list_path(recipe.id)
       expect(page).to have_content('Food')
+      expect(page).to have_content('Chicken Breast')
     end
 
     it 'Shows the quantity of food to buy' do
+      recipe
+      food
+      recipe_food
       visit recipe_general_shopping_list_path(recipe.id)
       expect(page).to have_content('Quantity')
+      expect(page).to have_content('2.0 Kg')
     end
 
     it 'Shows the price of each food to buy' do
+      recipe
+      food
+      recipe_food
       visit recipe_general_shopping_list_path(recipe.id)
       expect(page).to have_content('Price')
+      expect(page).to have_content('200.0')
     end
   end
 end
