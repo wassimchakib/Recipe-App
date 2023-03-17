f74e5ac595fc5e824d3b648de01419fcede12448
Wassim <wassimchakib@hotmail.com>
2023-03-17 19:02:35 +0100

    Fix Add ingredients page

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,5 +4,5 @@ class Food < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 3 }
   validates :price, presence: true, numericality: { greater_than: 0 }
-  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
