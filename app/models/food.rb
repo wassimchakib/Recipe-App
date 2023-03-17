7198156de416b2d1ed9815ac92ee7ec861e1167e
Ben Sales <adebsa2401@gmail.com>
2023-03-17 12:23:03 +0100

    spec: Add specs for food

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,4 +3,6 @@ class Food < ApplicationRecord
   has_many :recipe_foods
 
   validates :name, presence: true, length: { minimum: 3 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let :user do
+    User.find_or_create_by(email: 'test@microverse.org') do |user|
+      user.name = 'micronaut'
+      user.password = Devise.friendly_token.first(8)
+      user.confirmed_at = Time.now
+    end
+  end
+
+  let :food do
+    Food.find_or_create_by(name: 'Olive Oil', measurement_unit: 'l', price: 200, quantity: 5, user: user)
+  end
+
+  it 'fails because name should be present' do
+    food.name = nil
+    expect(food).to_not be_valid
+  end
+
+  it 'fails because price should be present' do
+    food.price = nil
+    expect(food).to_not be_valid
+  end
+
+  it 'fails because quantity should be present' do
+    food.quantity = nil
+    expect(food).to_not be_valid
+  end
 end
