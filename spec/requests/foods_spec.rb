f1be49a20076ab4bfaa408504de81bee935d11e6
Wassim <wassimchakib@hotmail.com>
2023-03-17 22:49:50 +0100

    Fix requests spec

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -1,31 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'FoodsController', type: :request do
-  describe 'GET /foods/' do
-    before(:all) do
-      user = User.find_by(email: 'wassimchakib@gmail.com')
-      user.skip_confirmation!
-      login_as(user)
-      get foods_path
-    end
-
-    # Get lists of all foods
-    it 'returns a 200 status response' do
-      expect(response).to be_successful
-      expect(response).to have_http_status(200)
+  let :user do
+    User.find_or_create_by(email: 'wassimchakib@gmail.com') do |user|
+      user.name = 'wassim'
+      user.password = Devise.friendly_token.first(8)
+      user.confirmed_at = Time.now
     end
   end
 
-  describe 'GET /foods/:food_id' do
-    before(:all) do
-      user = User.find_by(email: 'wassimchakib@gmail.com')
-      user.skip_confirmation!
-      login_as(user)
-      get recipe_path(1)
-    end
+  before(:each) do
+    login_as(user)
+  end
 
-    # Get Specific Recipe
+  describe 'GET /foods/' do
+    # Get lists of all foods
     it 'returns a 200 status response' do
+      get foods_path
       expect(response).to be_successful
       expect(response).to have_http_status(200)
     end
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,31 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'RecipesController', type: :request do
-  describe 'GET /recipes/' do
-    before(:all) do
-      user = User.find_by(email: 'wassimchakib@gmail.com')
-      user.skip_confirmation!
-      login_as(user)
-      get recipes_path
+  let :user do
+    User.find_or_create_by(email: 'wassimchakib@gmail.com') do |user|
+      user.name = 'wassim'
+      user.password = Devise.friendly_token.first(8)
+      user.confirmed_at = Time.now
     end
+  end
 
+  let(:recipe) do
+    Recipe.create!(user:, name: 'Grilled Basil Chicken', preparation_time: '10 min', cooking_time: '45 min',
+                   public: true, description: 'Wash hands with soap and water.')
+  end
+
+  before(:each) do
+    login_as(user)
+    recipe
+  end
+
+  describe 'GET /recipes/' do
     # Get lists of all recipes
     it 'returns a 200 status response' do
+      get recipes_path
       expect(response).to be_successful
       expect(response).to have_http_status(200)
     end
   end
 
   describe 'GET /recipes/:recipe_id' do
-    before(:all) do
-      user = User.find_by(email: 'wassimchakib@gmail.com')
-      user.skip_confirmation!
-      login_as(user)
-      get recipe_path(1)
-    end
-
     # Get Specific Recipe
     it 'returns a 200 status response' do
+      get recipe_path(recipe.id)
       expect(response).to be_successful
       expect(response).to have_http_status(200)
     end
